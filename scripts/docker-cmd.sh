37a4eba67ec457220caa0b3161862bce7d5eb756
Zicklag <zicklag@katharostech.com>
2018-10-25 23:58:13 +0000

    Add INIT_BACKUP Environment Variable

--- scripts/docker-cmd.sh
+++ scripts/docker-cmd.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+backup_cmd="mongodump --host $MONGO_HOST --port $MONGO_PORT --archive=/backup/db-backup.archive.gz --gzip"
+
+echo "#!/bin/bash" > /run-backup.sh
+echo "$backup_cmd" > /run-backup.sh
+
+if [ "$INIT_BACKUP" = "true" ]; then
+    echo "Running Init Backup"
+    bash /run-backup.sh
+fi
+
 # Create cron job
-echo "$CRON_SCHEDULE mongodump --host $MONGO_HOST --port $MONGO_PORT --archive --gzip > /backup/db-backup.archive.gz" > /cron-jobs.txt
+echo "$CRON_SCHEDULE bash /run-backup.sh" > /cron-jobs.txt
 
 # Start Cron
 exec supercronic /cron-jobs.txt
